dd39efac901bb0ce09d91d7947e113ca22fa70e2
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-12 12:15:42 -0500

    create componentDidMount; get data from db via axios request

--- client/index.js
+++ client/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const contacts = [
-  {"id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com"},
-  {"id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com"},
-  {"id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com"}
-]
 class Main extends Component {
   constructor() {
     super();
-    this.state = { contacts: contacts };
+    this.state = { contacts: [] };
+  }
+
+  async componentDidMount() {
+    const { data } = await axios.get('/api/contacts');
+    console.log('======>', data);
+    this.setState(() => ({ contacts: data }))
   }
 
   render() {
